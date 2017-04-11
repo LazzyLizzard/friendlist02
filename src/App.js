15052ff757e579748f157ddf33454b9e473509c3
sf <lazy.lizard@yandex.ru>
2017-04-11 17:29:05 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import testA from './components/_tempFriends'
+
+
 export default class App extends Component {
-  render() {
-    return (
-      <h1>Hello, world.</h1>
-    );
-  }
+    render() {
+        return (
+
+            //<div>234</div>
+            <testA />
+        );
+    }
 }
---
+++ src/components/_tempFriends.js
@@ -0,0 +1,8 @@
+import React, { Component, PropTypes } from 'react';
+
+class User extends React.Component {
+    render () {
+        return <div>User</div>;
+    }
+}
+export default User;
