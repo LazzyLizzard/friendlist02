dbbb8cc80ba32660901b47964611ea0b531dbe63
sf <lazy.lizard@yandex.ru>
2017-04-11 19:11:33 +0300

    render list fail 3

--- src/components/userItem.js
+++ src/components/userItem.js
@@ -4,11 +4,11 @@ class UserListItem extends Component {
 
     render() {
 
-        let ul = this.props.listToPass;
+        let ul = this.props.listToPass.list;
 
         return <div>
             Object.keys(ul).map((key) => {
-            <li>{{ ul[key].name }}</li>
+            <li>{{ul[key].name}}</li>
         }
         </div>
 
