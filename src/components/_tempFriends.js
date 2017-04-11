da9db6f63cb5c3ff728ad988720c5f4d468be2b2
sf <lazy.lizard@yandex.ru>
2017-04-11 17:39:34 +0300

    list

--- src/components/_tempFriends.js
+++ src/components/_tempFriends.js
@@ -1,6 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 
-class User extends Component {
+const userList = {
+
+    list: {
+        1: {
+            name: 'john',
+            lastName: 'doe'
+        },
+        2: {
+            name: 'john',
+            lastName: 'doe'
+        }
+    }
+}
+
+class User extends React.Component {
     render () {
         return <div>User aaa</div>;
     }
